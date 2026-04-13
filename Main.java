cdce90625569957a79cf2e037e111fbc9128d552
Vyygeyl <almadinabby@gmail.com>
2026-04-13 12:53:57 +0800

    Added browsePage (linked w/ prdctDetails)

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proj_OOP;
+
+/**
+ *
+ * @author pc
+ */
+
+public class Main{
+    public static void main (String[]args ){
+    
+    browsePage mainPage = new browsePage();
+    mainPage.setVisible(true);
+}
+}
